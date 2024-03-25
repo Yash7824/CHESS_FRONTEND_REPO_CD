55edd642ed556d5522c18fe470a4d53c7d9f79c2
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-03-26 01:27:45 +0530

    merge-conflict: Bringing Yash's changes

--- client/src/app/pages/game-room/game-room.component.ts
+++ client/src/app/pages/game-room/game-room.component.ts
@@ -17,6 +17,10 @@ export class GameRoomComponent {
   @ViewChildren('column') columns!: QueryList<ElementRef>;
   @ViewChild('whitePawn') whitePawnRow!: ElementRef;
 
+  IsWhiteKingChecked: string = '';
+  IsBlackKingChecked: string = '';
+  hasWhiteKingMoved!: boolean
+  hasBlackKingMoved!: boolean
   currentPlayer: string = 'white';
 
   chessPieces: any = {};
@@ -50,6 +54,8 @@ export class GameRoomComponent {
 
   ngOnInit() {
     // console.log(this.gpElementRef?.nativeElement);
+    this.hasWhiteKingMoved = false;
+    this.hasBlackKingMoved = false;
     this.receiveJoinedPlayers();
   }
 
@@ -231,6 +237,194 @@ export class GameRoomComponent {
       piece == 'p'
     )
       return true;
+    return false;
+  }
+
+  IsWhiteKingUnderCheck(
+    king: string,
+    fromRow: number,
+    fromCol: number
+  ): boolean {
+    // The Piece giving check to the King
+    const piece = this.chess_Board[fromRow][fromCol];
+    let kingRow!: number;
+    let kingCol!: number;
+
+    // King's location:
+    for (let i = 0; i < this.chess_Board.length; i++) {
+      for (let j = 0; j < this.chess_Board[i].length; j++) {
+        if (this.chess_Board[i][j] == king) {
+          kingRow = i;
+          kingCol = j;
+          break;
+        }
+      }
+    }
+
+    if (king == 'K') {
+      // If Black pawn is giving Check.
+      if (
+        piece == 'p' &&
+        ((kingRow == fromRow + 1 && kingCol == fromCol - 1) ||
+          (kingRow == fromRow + 1 && kingCol == fromCol + 1))
+      ) {
+        return true;
+      }
+
+      // If Black Rook is giving Check
+      if (
+        piece == 'r' &&
+        ((kingRow != fromRow && kingCol == fromCol) ||
+          (kingRow == fromRow && kingCol != fromCol)) &&
+        !this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)
+      ) {
+        return true;
+      }
+
+      // If Black knight is giving Check.
+      if (
+        piece == 'n' &&
+        ((kingRow == fromRow - 2 &&
+          (kingCol == fromCol + 1 || kingCol == fromCol - 1)) ||
+          (kingRow == fromRow + 2 &&
+            (kingCol == fromCol + 1 || kingCol == fromCol - 1)) ||
+          (kingCol == fromCol - 2 &&
+            (kingRow == fromRow + 1 || kingRow == fromRow - 1)) ||
+          (kingCol == fromCol + 2 &&
+            (kingRow == fromRow + 1 || kingRow == fromRow - 1)))
+      ) {
+        return true;
+      }
+
+      // If Black bishop is giving Check
+      if (piece == 'b') {
+        for (let i = 1; i <= 7; i++) {
+          if (!this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)) {
+            if (
+              (kingRow == fromRow - i && kingCol == fromCol - i) ||
+              (kingRow == fromRow - i && kingCol == fromCol + i) ||
+              (kingRow == fromRow + i && kingCol == fromCol - i) ||
+              (kingRow == fromRow + i && kingCol == fromCol + i)
+            ) {
+              return true;
+            }
+          }
+        }
+      }
+
+      // If Black Queen is giving Check
+      if (piece == 'q') {
+        for (let i = 1; i <= 7; i++) {
+          if (!this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)) {
+            if (
+              (kingRow == fromRow - i && kingCol == fromCol - i) ||
+              (kingRow == fromRow - i && kingCol == fromCol + i) ||
+              (kingRow == fromRow + i && kingCol == fromCol - i) ||
+              (kingRow == fromRow + i && kingCol == fromCol + i) ||
+              (kingRow != fromRow && kingCol == fromCol) ||
+              (kingRow == fromRow && kingCol != fromCol)
+            ) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  IsBlackKingUnderCheck(
+    king: string,
+    fromRow: number,
+    fromCol: number
+  ): boolean {
+    // The Piece giving check to the King
+    const piece = this.chess_Board[fromRow][fromCol];
+    let kingRow!: number;
+    let kingCol!: number;
+
+    // King's location:
+    for (let i = 0; i < this.chess_Board.length; i++) {
+      for (let j = 0; j < this.chess_Board[i].length; j++) {
+        if (this.chess_Board[i][j] == king) {
+          kingRow = i;
+          kingCol = j;
+          break;
+        }
+      }
+    }
+
+    if (king == 'k') {
+      // If White pawn is giving Check.
+      if (
+        piece == 'P' &&
+        ((kingRow == fromRow - 1 && kingCol == fromCol - 1) ||
+          (kingRow == fromRow - 1 && kingCol == fromCol + 1))
+      ) {
+        return true;
+      }
+
+      // If White Rook is giving Check
+      if (
+        piece == 'R' &&
+        ((kingRow != fromRow && kingCol == fromCol) ||
+          (kingRow == fromRow && kingCol != fromCol)) &&
+        !this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)
+      ) {
+        return true;
+      }
+
+      // If White knight is giving Check.
+      if (
+        piece == 'N' &&
+        ((kingRow == fromRow - 2 &&
+          (kingCol == fromCol + 1 || kingCol == fromCol - 1)) ||
+          (kingRow == fromRow + 2 &&
+            (kingCol == fromCol + 1 || kingCol == fromCol - 1)) ||
+          (kingCol == fromCol - 2 &&
+            (kingRow == fromRow + 1 || kingRow == fromRow - 1)) ||
+          (kingCol == fromCol + 2 &&
+            (kingRow == fromRow + 1 || kingRow == fromRow - 1)))
+      ) {
+        return true;
+      }
+
+      // If White bishop is giving Check
+      if (piece == 'B') {
+        for (let i = 1; i <= 7; i++) {
+          if (!this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)) {
+            if (
+              (kingRow == fromRow - i && kingCol == fromCol - i) ||
+              (kingRow == fromRow - i && kingCol == fromCol + i) ||
+              (kingRow == fromRow + i && kingCol == fromCol - i) ||
+              (kingRow == fromRow + i && kingCol == fromCol + i)
+            ) {
+              return true;
+            }
+          }
+        }
+      }
+
+      // If White Queen is giving Check
+      if (piece == 'Q') {
+        for (let i = 1; i <= 7; i++) {
+          if (!this.IsInvalidMove(piece, fromRow, fromCol, kingRow, kingCol)) {
+            if (
+              (kingRow == fromRow - i && kingCol == fromCol - i) ||
+              (kingRow == fromRow - i && kingCol == fromCol + i) ||
+              (kingRow == fromRow + i && kingCol == fromCol - i) ||
+              (kingRow == fromRow + i && kingCol == fromCol + i) ||
+              (kingRow != fromRow && kingCol == fromCol) ||
+              (kingRow == fromRow && kingCol != fromCol)
+            ) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+
     return false;
   }
 

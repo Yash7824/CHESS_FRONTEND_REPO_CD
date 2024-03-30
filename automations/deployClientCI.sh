83bfdc619c1a35edd777c8ed033826d6e29465a5
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-03-31 01:37:22 +0530

    fix: remove second netlify

--- automations/deployClientCI.sh
+++ automations/deployClientCI.sh
@@ -8,7 +8,7 @@ echo "Create Build File..."
 ng build 
 echo "Deploy Build with Netlify CLI..."
 npm install netlify-cli -g
-netlify netlify deploy \
+netlify deploy \
             --dir dist \
             --site "$NETLIFY_SITE_ID"  \
             --auth "$NETLIFY_API_TOKEN"

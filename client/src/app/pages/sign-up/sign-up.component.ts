939e9c259612c91507686dab3d1c576cdeb0eef2
Yash <yashr7824@gmail.com>
2024-05-04 22:18:12 +0530

    refactor: revamped signUp page

--- client/src/app/pages/sign-up/sign-up.component.ts
+++ client/src/app/pages/sign-up/sign-up.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -13,18 +21,26 @@ export class SignUpComponent {
     private router: Router
   ){}
 
-  signUpForm = {
-    name: '',
-    email: '',
-    password: '',
-    repeat_password: ''
-  };
+  nameFormControl = new FormControl('', [Validators.required])
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
+  repeatPasswordFormControl = new FormControl('', [Validators.required]);
+  hide1 = true;
+  hide2 = true;
+  matcher = new MyErrorStateMatcher();
 
   signUp(){
-    this.apiServ.signUp(this.signUpForm).subscribe({
+    let signUpForm = {
+      name: this.nameFormControl.value,
+      email: this.emailFormControl.value,
+      password: this.passwordFormControl.value,
+      repeat_password: this.repeatPasswordFormControl.value
+    }
+
+    this.apiServ.signUp(signUpForm).subscribe({
       next: (response) => {
         sessionStorage.setItem('signUpToken', JSON.stringify(response));
-        this.router.navigateByUrl('/login')
+        this.router.navigateByUrl('')
       },
       error: err => console.error('Observable emitted an error: ' + err)
       // complete: () => console.log('Observable emitted the complete notification')

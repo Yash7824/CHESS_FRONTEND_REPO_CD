033e38ac23e38b38eff4c4ba3102f5e6f477170f
Yash <yashr7824@gmail.com>
2024-08-25 21:19:40 +0530

    refactor: Repo specific for FE

--- server/dto/UpdateUser.ts
+++
@@ -1,5 +0,0 @@
-export interface UpdateUser {
-    name: string
-    password: string
-    confirmPassword: string
-}
--- server/modules/updateMovementList.ts
+++
@@ -1,8 +0,0 @@
-import { Socket, Server } from "socket.io";
-import { Room } from "../interfaces/room";
-
-const updateMovementList = (io: Server, socket: Socket, updatedMovementList: any, roomName: string, activeRooms: Map<string, Room>, socketIDToUserNameMapper: Map<string,string>) => {
-    io.to(roomName).emit('updatedMovement', updatedMovementList);
-}
-
-export default updateMovementList;

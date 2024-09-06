dcd382878a123aec9d45d3a066ab91654f5507a6
Yash <yashr7824@gmail.com>
2024-09-07 01:11:09 +0530

    fix: Prod Pointing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     prod: true,
     // base_url: 'http://localhost:3000/api',
-    base_url: 'https://chess-xtreme-services.onrender.com',
+    base_url: 'https://chess-xtreme-services.onrender.com/api',
 }
